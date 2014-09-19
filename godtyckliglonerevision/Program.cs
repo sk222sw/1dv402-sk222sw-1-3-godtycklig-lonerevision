13b346d9f60d8ce5ce14008ee1013a0248112671
Sonny <sonnykjellberg@gmail.com>
2014-09-19 15:45:14 +0200

    Added comments to structure the laboration.

---
+++ godtyckliglonerevision/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace godtyckliglonerevision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Deklarerar variabler
+                    //Array med antal löner
+        uint numberOfSalaries;
+
+
+        //Antal löner att mata in: 
+
+        //Ange lön nummer int
+
+        Console.WriteLine("-----------------------");
+        //Skriv ut medianlön
+
+        //Medellön
+
+        //Lönespridning
+        Console.WriteLine("-----------------------");
+
+        //Skriv ut lönerna, tre per rad
+
+        }/////////// Main slut
+
+
+        //Två metoder:
+
+        //ReadInt() = användaren matar in antal löner. Användaren matar in löner. 
+            //Felmeddelande om färre än 2.
+            //Felmeddelande om det inte är en uint
+            //Anropa ProcessSalaries() med antal löner som argument
+
+        static int ReadInt
+        { 
+            nummerIReadInt = Console.ReadLine();
+        }
+
+
+        //ProcessSalaries() = läs in antalet löner. -> beräkna median, medell och lönespridning. -> skriv ut lönerna under. 
+    }
+}
